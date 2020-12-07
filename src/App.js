40ea6f1215fb40d2a7f16fda71684dd63e929b6f
zyttor <zyttor@gmail.com>
2020-12-07 12:28:06 -0600

    Estructura Header.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      
+      {/** Header */}
+      <Header />
+      {/** Tinder Cards*/}
+      {/** Tinder Buttons  */}
+      
+
+    </div>
+  );
+}
+
+export default App;
